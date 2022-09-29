d320d2374b07093bc90db14b16f7550f274111be
aftab87 <0514314@johnabbottcollege.net>
2022-09-29 12:05:53 -0400

    Revert "Revert "Merge branch 'registration'""
    This reverts commit 8c3e1181896bea057c6e7908b3271dfafb070b4b.

--- incl/logic/signup.php
+++ incl/logic/signup.php
@@ -1,4 +1,5 @@
 <?php
+require_once ("incl/logic/sanitize.php");
 
 $trn_date = date("Y-m-d H:i:s");
 
@@ -30,8 +31,6 @@ function ValidateForm()
                 $isValid = false;
             }
         }
-
-
         if (empty($_POST["password"])) {
             $password_error = "Password is required";
             $isValid = false;
@@ -59,23 +58,10 @@ function ValidateForm()
     }
 }
 
-function sanitize($data)
-{
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
-}
+
 
 ValidateForm();
 
-
-
-
-
-
-// $query = "INSERT into `users` (username, password, email, trn_date)
-//     VALUES ('$username', '" . md5($password) . "', '$email', '$trn_date')";
 if ($isValid) {
     $qry = "INSERT into `users` (username, password, email)
                 VALUES (?,?,?) ";
@@ -84,40 +70,51 @@ if ($isValid) {
     $password = md5($password);
 
     mysqli_stmt_bind_param($stmt, "sss", $username, $password, $email);
-  
+
     try {
         $result = mysqli_stmt_execute($stmt);
     } catch (Exception $e) {
         if ($e->getCode() == 1062) {
             if (str_contains($e->getMessage(), "username")) {
-                $username_error = "this username is already taken";               
-            }else if (str_contains($e->getMessage(), "email")) {
+                $username_error = "this username is already taken";
+            } else if (str_contains($e->getMessage(), "email")) {
                 $email_error = "this email is already taken";
             }
         }
-        // if($e->getCode() == 1062){
-        //     $username_error= "this username is already taken";
-        // }
-
-
-        // $email_error = $e->getMessage();
-
-    }
-
-    // $result = mysqli_query($conn,$query);
-    if (!empty($result)) {
-        // $findId = "SELECT id from users where username = '.$username.'";
-        // $result2 = mysqli_query($conn,$findId);
-        // $row = mysqli_fetch_assoc($result2);
-
-
-
-
-        $_SESSION['username'] = $username;
-        // $_SESSION['user_id'] = $row[0];
-        header('location: index.php');
     }
 }
+
+
+// $result = mysqli_query($conn,$query);
+    if (!empty($result)) {
+        $qry = "SELECT id, username, password FROM users WHERE username=?";
+        $stmt = mysqli_prepare($conn, $qry);
+        mysqli_stmt_bind_param($stmt, "s", $username);
+        mysqli_stmt_execute($stmt) or die("Server error : login failed");
+        mysqli_stmt_store_result($stmt);
+        if (mysqli_stmt_num_rows($stmt) == 1) {
+
+            mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
+            mysqli_stmt_fetch($stmt);
+
+                //get user data
+                $user = [
+                    "username" => $username,
+                    "user_id" => $id
+                ];
+                
+                //save used data to session 
+                //TODO : set session timeout (ex: 30 min) and extend that every time tehre is a request
+                //reference: https://stackoverflow.com/questions/520237/how-do-i-expire-a-php-session-after-30-minutes
+                $_SESSION['user'] =  $user;
+                $_SESSION['username'] = $user["username"];
+                $_SESSION['user_id'] = $user["user_id"];
+
+                mysqli_stmt_close($stmt);
+        header('location: index.php');
+    
+}
+    }
 ?>
 <?php
 //Show Modal on reload on incorrect login
@@ -130,4 +127,4 @@ if (isset($_POST['signup'])) : ?>
             myModal.show();
         };
     </script>
-<?php endif; ?>
+    //<?php endif; ?>
