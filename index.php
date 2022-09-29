216562c6fe7aee7f35cb2ad085e72b2130489a12
zac <zacclarke@gmail.com>
2022-09-29 18:11:43 -0400

    index work

--- index.php
+++ index.php
@@ -1,18 +1,49 @@
 <?php require "incl/parts/header.php"; ?>
-        <main class="px-3">
+<main class="px-3">
 
-        
-            
-                <?php if (isset($_SESSION['username'])) :?>
-                        <h1>Hello <?= $_SESSION['username']?></h1>
-                        <p class="pt-5">You are  logged in. Try to go to your DASHBOARD now.</p>
-                        
-                    <?php else :?>
-                        <h1>Hello Stranger</h1>
-                        <p class="pt-5">You are  NOT logged in. Try to login now.</p>
-                    <?php endif ?>
-         
-           
-            
-        </main>
+    <div class="mt-5">
+        <h1>Annotate your favorite YouTube videos!</h1>
+    </div>
+    <div class="mt-5 mb-5 container w-75">
+        <p>
+            Long have we waited for a web service which allowed us to add notes to the videos we use
+            to study! Sign up now to use are indespensible web application to help you keep track of
+            what you're watching and what you're learning!
+        </p>
+    </div>
+    <div class="mb-5 container">
+        <h2>How does it work?</h2>
+    </div>
+
+    <div class="container">
+
+        <div class="row">
+            <div class=" col">
+                <img src="img/addVideo.png" alt="add video modal" width=400>
+                <h3>Add videos!</h3>
+                <p>
+                    Using a youtube link you can upload videos to your profile,
+                    allowing you to keep track of videos you need to study!
+                </p>
+            </div>
+            <div class=" col">
+                <img src="img/video-placeholder.jpg" width=400 height=242>
+                <h3>Add notes to videos!</h3>
+                <p>
+                    You will be able to add a notes to videos! This not will
+                    keep the time in the video that you wrote it!
+                </p>
+            </div>
+            <div class=" col">
+                <img src="img/video-placeholder.jpg" width=400 height=242>
+                <h3>Review your notes!</h3>
+                <p>
+                    All notes will be saved with the video you took them during. you will be able to review
+                    them all on one page. You can even set the video back to where you took the note with on click!
+                </p>
+            </div>
+        </div>
+    </div>
+
+</main>
 <?php require "incl/parts/footer.php"; ?>
